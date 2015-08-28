6902ff40879fd660fedcafdf77b96600f329191b
pmelab <philipp.melab@gmail.com>
2015-08-28 07:47:16 +0200

    Another packaging fix.

--- setup.py
+++ setup.py
@@ -7,15 +7,6 @@ https://github.com/pypa/sampleproject
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='dork',
@@ -23,10 +14,9 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',
+    version='0.1.2',
 
     description='Docker and ansible based development containers.',
-    long_description=long_description,
 
     # The project's main homepage.
     url='https://github.com/iamdork/dork',
