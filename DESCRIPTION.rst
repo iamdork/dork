64cfe5efdea603b4fa8016bfc6875bac95ce8a61
pmelab <philipp.melab@gmail.com>
2015-08-28 07:14:24 +0200

    Pip packaging.

--- DESCRIPTION.rst
+++ DESCRIPTION.rst
@@ -2,4 +2,4 @@ Welcome to the DORK side
 ========================
 
 Dork - also called the `Docker Fork` - is a wrapper script around docker and
-git, to ease handling of development environments and testing data.
+git, to ease handling of web development environments and runtime data.
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.1',
 
     description='Dork',
     long_description=long_description,
@@ -60,7 +60,7 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='docker devops provisioning',
+    keywords='docker devops development ansible',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
